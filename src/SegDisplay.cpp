00bec5f69b46f8b3b7d03540ccb8a70f10bd33a0
BlazinBaggins <cjohnston20@my.whitworth.edu>
2023-01-12 10:33:56 -0800

    fixed seg1 bus out

--- src/SegDisplay.cpp
+++ src/SegDisplay.cpp
@@ -1,6 +1,6 @@
 #include "SegDisplay.h"
 
-BusOut Seg1(p5,p5,p6,p7,p8,p9,p10,p11); // a-g (no dp)
+BusOut Seg1(p5,p6,p7,p8,p9,p10,p11); // a-g (no dp)
 BusOut Seg2(p13,p14,p15,p16,p17,p18,p19); // a-g (no dp)
 
 void SegInit(void){
